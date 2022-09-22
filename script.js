48591099be17340eccecfe24c6d0bc7945558856
TalesCostta <talessilvacostta@gmail.com>
2022-09-22 10:53:33 -0300

    Alterado o script do bot

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-var url_string = window.location.href;
-var url = new URL(url_string);
-var acessChat = url.searchParams.get("acessChat");
-
-if (acessChat != null) {
-    localStorage.setItem('chatbot-info-student', acessChat);
-}
-
 var paramChat;
-paramChat = localStorage.getItem('chatbot-info-student')
+paramChat = window.location.href;
 let cronchatToken = '5203171793:AAFWMcOzQaKHbVKft3QN3ndbcNYyLlgDEIo';
 let hostURL = 'https://cronchatcronapp.cronapp.io/get-chat?token=';
 let chatbotURL = `${hostURL}${cronchatToken}&extraparam=${paramChat}`;
@@ -15,16 +7,3 @@ let scriptHTML = document.createElement("script");
 scriptHTML.src = chatbotURL;
 scriptHTML.type = 'text/javascript'
 document.body.appendChild(scriptHTML);
-
-window.onload = function () {
-    if (acessChat != null) {        
-        setTimeout(function(){
-            clickBtnChat()
-          }, 1000);
-    }
-}
-
-function clickBtnChat(){
-    var btnConversationId = 'chatBtn_' + localStorage.getItem('conversationidbot');
-    document.getElementById(btnConversationId).click();
-}
