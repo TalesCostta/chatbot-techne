cc6ad9a1c89c9cbfdd26f14e22846748e9724ad7
Tales Costa <48179011+TalesCostta@users.noreply.github.com>
2023-10-31 08:45:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ if (acessChat != null) {
 var paramChat;
 paramChat = localStorage.getItem('chatbot-info-student')
 let cronchatToken = 'AAGu0cX1U7HIuyWyB6aA1HhpD4l0c0psnTw';
-let hostURL = 'https://cronchatcronapp.cronapp.io/get-chat?token=';
+let hostURL = 'https://cronchatnovo.cronapp.io/get-chat?token=';
 let chatbotURL = `${hostURL}${cronchatToken}&extraparam=${paramChat}`;
 let scriptHTML = document.createElement("script");
 scriptHTML.src = chatbotURL;
